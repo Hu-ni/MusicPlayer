f6687e0444dff2168040185ceac013d3981d537a
Hu-ni <dev-game@naver.com>
2020-02-18 22:16:23 +0900

    Add Project

---
+++ MusicPlayer/MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listView1.Items.Add(new CustomerInfo() 
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" });
+            listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" });
+            listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" });
+            listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" }); listView1.Items.Add(new CustomerInfo()
+            { Memo = "1", Subject = "2", Class = "3", Professor = "4", ScheDule = "5" });
+            listView1.SelectedIndex = 1;
+        }
+
+
+        //리스트뷰에서 선택한 항목의 정보를 얻어온다.
+        private static CustomerInfo ListView_GetItem(RoutedEventArgs e)
+        {
+            DependencyObject dep = (DependencyObject)e.OriginalSource;
+
+            while (!(dep is System.Windows.Controls.ListViewItem))
+            {
+                try
+                {
+                    dep = VisualTreeHelper.GetParent(dep);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            ListViewItem item = (ListViewItem)dep;
+            CustomerInfo content = (CustomerInfo)item.Content;
+
+            return content;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerInfo info = ListView_GetItem(e);
+        }
+
+        private void listView1_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class CustomerInfo
+    {
+        private string _Memo;
+        private string _Subject;
+        private string _Class;
+        private string _Professor;
+        private string _ScheDule;
+
+        public string Memo { get { return _Memo; } set { _Memo = value; } }
+        public string Subject { get { return _Subject; } set { _Subject = value; } }
+        public string Class { get { return _Class; } set { _Class = value; } }
+        public string Professor { get { return _Professor; } set { _Professor = value; } }
+        public string ScheDule { get { return _ScheDule; } set { _ScheDule = value; } }
+    }
+}
---
+++ 기획서/README.md
@@ -0,0 +1,40 @@
+# 음악플레이어 기획
+
+### 요구사항
+
+1. 미디어 관련
+   1. 음악 재생
+   2. 음악 정지
+   3. 음량 조절
+   4. 재생 중인 음악 정보 표시
+   5. 재생 위치 표시
+   6. 재생 위치 선택 시 해당 위치부터 재생
+2. 목록 관리
+   1. 음악 추가
+   2. 음악 삭제
+   3. 음악 검색
+   4. 전체 음악목록 조회
+
+### 구조 설계
+
+- 플로우차트
+
+<img src="./기능\Media.png"/>
+
+<img src="./기능/MusicList.png"/>
+
+- 클래스 다이어그램
+
+<img src="./기능/ClassDiagram.png"/>
+
+### 인터페이스 설계
+
+<img src="./인터페이스\UInUX.png"/>
+
+<img src="./인터페이스\UI Details.png"/>
+
+
+
+### 출처
+
+인터페이스 아이콘:  <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com (<a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn</a>)</a>
